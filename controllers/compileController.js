55868143a33391926fe3e33beb1e99338a64348f
Davide Di Francesco <davdifr@outlook.com>
2024-05-20 17:51:21 +0200

    chore: Update compileController.js and server.js

--- controllers/compileController.js
+++ controllers/compileController.js
@@ -7,13 +7,29 @@ exports.compileScss = (req, res) => {
         return res.status(400).send('No files uploaded.');
     }
 
-    const scssContent = req.files
-        .map((file) => fs.readFileSync(file.path, 'utf8'))
-        .join('\n');
+    const filesContent = {};
+
+    req.files.forEach((file) => {
+        filesContent[file.originalname] = fs.readFileSync(file.path, 'utf8');
+    });
+
+    const mainScssContent =
+        Object.keys(filesContent)
+            .map((filename) => {
+                return `@import "${filename}";`;
+            })
+            .join('\n') +
+        '\n' +
+        filesContent['styles.scss'];
 
     try {
-        const result = sass.renderSync({ data: scssContent });
+        const result = sass.renderSync({
+            data: mainScssContent,
+            includePaths: [tmpdir()],
+        });
+
         req.files.forEach((file) => fs.unlinkSync(file.path));
+
         res.type('text/css').send(result.css);
     } catch (error) {
         req.files.forEach((file) => fs.unlinkSync(file.path));
--- server.js
+++ server.js
@@ -6,4 +6,7 @@ const app = express();
 
 app.post('/api/compile-scss', uploadMiddleware, compileController.compileScss);
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
